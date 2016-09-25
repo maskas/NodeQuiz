5fbf6e8bcec30dd080e4b47d186bbf08e9fd9204
Mangirdas Skripka <mangirdas@impresspages.org>
2016-09-25 19:33:14 +0300

    use pug templating system

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,10 @@
 var express = require('express'),
     repository = require('./../repository'),
     router = express.Router(),
+    pug = require('pug'),
     json = require('json'),
     React = require('react'),
     ReactDOMServer = require('react-dom/server'),
-    DOM = React.DOM,
-    html = DOM.html,
-    body = DOM.body,
-    div = DOM.div,
-    script = DOM.script,
-    link = DOM.link,
-    head = DOM.head,
-    meta = DOM.meta,
-    h1 = DOM.h1;
 
 Quiz = React.createFactory(require('./../component/App'));
 
@@ -24,44 +16,11 @@ router.get('/', function(req, res) {
         currentQuestion: 0
     };
 
-    var htmlContent = ReactDOMServer.renderToStaticMarkup(html(null,
-        head(null,
-            meta({
-                charSet: 'utf-8'
-            }),
-            link({
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: '/bootstrap.min.css'
-            }),
-            link({
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: '/style.css'
-            })
-        ),
-        body(null,
-            div({className: 'page-header'}, h1(null, 'JS GURU QUIZ')),
-
-            div({
-                id: 'content',
-                className: 'wrapper',
-                dangerouslySetInnerHTML: {
-                    __html: ReactDOMServer.renderToString(Quiz(props))
-                }
-            }),
-
-            script({
-                dangerouslySetInnerHTML: {
-                    __html: 'var APP_PROPS = ' + safeStringify(props) + ';'
-                }
-            }),
-
-            script({src: '//cdnjs.cloudflare.com/ajax/libs/react/15.3.0/react.min.js'}),
-            script({src: '//cdnjs.cloudflare.com/ajax/libs/react/15.3.0/react-dom.min.js'}),
-
-            script({src: '/bundle.js'})
-        )));
+    var htmlContent = pug.renderFile('./views/index.pug', {
+        pageTitle: 'JS GURU QUIZ',
+        props: safeStringify(props),
+        content: ReactDOMServer.renderToString(Quiz(props))
+    });
 
     res.end(htmlContent)
 });
