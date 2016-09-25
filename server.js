8e0aa7db24fec2f856ac887e4dcdc1d7dda5df48
Mangirdas Skripka <mangirdas@impresspages.org>
2016-09-25 15:57:42 +0300

    controller

--- server.js
+++ server.js
@@ -6,20 +6,12 @@ var http = require('http'),
     json = require('json'),
     literalify = require('literalify'),
     React = require('react'),
-    ReactDOMServer = require('react-dom/server'),
     DOM = React.DOM,
-    html = DOM.html,
     body = DOM.body,
-    div = DOM.div,
-    script = DOM.script,
-    link = DOM.link,
-    head = DOM.head,
-    meta = DOM.meta,
-    h1 = DOM.h1,
     querystring = require('querystring'),
     repository = require('./repository'),
+    rootController = require('./controller/root.js'),
 
-    Quiz = React.createFactory(require('./component/App'));
 app = new express();
 
 
@@ -28,55 +20,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'text/html');
-
-    var props = {
-        questions: repository.getQuestions(),
-        currentQuestion: 0
-    };
-
-    var htmlContent = ReactDOMServer.renderToStaticMarkup(html(null,
-        head(null,
-            meta({
-                charSet: 'utf-8'
-            }),
-            link({
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: '/bootstrap.min.css'
-            }),
-            link({
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: '/style.css'
-            })
-        ),
-        body(null,
-            div({className: 'page-header'}, h1(null, 'JS GURU QUIZ')),
-
-            div({
-                id: 'content',
-                className: 'wrapper',
-                dangerouslySetInnerHTML: {
-                    __html: ReactDOMServer.renderToString(Quiz(props))
-                }
-            }),
-
-            script({
-                dangerouslySetInnerHTML: {
-                    __html: 'var APP_PROPS = ' + safeStringify(props) + ';'
-                }
-            }),
-
-            script({src: '//cdnjs.cloudflare.com/ajax/libs/react/15.3.0/react.min.js'}),
-            script({src: '//cdnjs.cloudflare.com/ajax/libs/react/15.3.0/react-dom.min.js'}),
-
-            script({src: '/bundle.js'})
-        )));
-
-    res.end(htmlContent)
-});
+app.get('/', rootController.handle);
 
 
 app.get('/bundle.js', function (req, res) {
@@ -117,7 +61,4 @@ if (process.env.SERVER_PORT) {
 }
 app.listen(port);
 
-// A utility function to safely escape JSON for embedding in a <script> tag
-function safeStringify(obj) {
-    return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
-}
+
